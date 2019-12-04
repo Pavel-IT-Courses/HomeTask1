c92c603cafde856b39ca9f06eef4f1e21dbb833a
paul <pavkascool@gmail.com>
2019-12-04 13:16:39 +0300

    EditContactActivity added

--- homework2/src/main/java/com/gmail/pavkascool/homework2/ContactBookActivity.java
+++ homework2/src/main/java/com/gmail/pavkascool/homework2/ContactBookActivity.java
@@ -1,15 +1,28 @@
 package com.gmail.pavkascool.homework2;
 
+import android.app.Service;
+import android.content.Intent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 
 import android.os.Bundle;
 
-public class ContactBookActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactBookActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static List<Person> persons;
 
     private Toolbar toolbar;
-
+    private SearchView searchView;
+    private Button next;
 
 
     @Override
@@ -18,9 +31,26 @@ public class ContactBookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact_book);
 
         toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.first);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
+        toolbar.requestFocus();
 
+        searchView = findViewById(R.id.search);
 
+        persons = new ArrayList<>();
+
+        next = findViewById(R.id.next);
+        next.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, EditContactActivity.class);
+        startActivity(intent);
     }
 }
---
+++ homework2/src/main/java/com/gmail/pavkascool/homework2/EditContactActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.pavkascool.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contact);
+    }
+}
